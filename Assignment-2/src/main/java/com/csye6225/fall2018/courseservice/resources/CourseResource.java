0f04de1ba26eaff92c6c1808c5e8e5eebb05347d
Manjula Bashini Prabhu <31738763+ManjulaPrabhu@users.noreply.github.com>
2018-11-12 16:18:53 -0800

    Uploaded Assignment-2 Solution

---
+++ Assignment-2/src/main/java/com/csye6225/fall2018/courseservice/resources/CourseResource.java
@@ -0,0 +1,60 @@
+package com.csye6225.fall2018.courseservice.resources;
+
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.csye6225.fall2018.courseservice.datamodel.Course;
+import com.csye6225.fall2018.courseservice.service.CourseService;
+
+//.. /webapi/myresource
+@Path("courses")
+
+public class CourseResource {
+	CourseService courseServiceObject = new CourseService();
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Course> getAllCourses() {
+		return courseServiceObject.getAllCourses();
+	}
+
+	// ... webapi/courses/1
+	@GET
+	@Path("/courses/{courseId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Course getCourse(@PathParam("courseId") long courseId) {
+		return courseServiceObject.getCourse(courseId);
+	}
+
+	@DELETE
+	@Path("/{courseId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Course deleteCourse(@PathParam("courseId") long courseId) {
+		return courseServiceObject.deleteCourse(courseId);
+	}
+
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Course addCourse(Course course) {
+		return courseServiceObject.addCourse(course);
+	}
+
+	@PUT
+	@Path("/{courseId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Course updateCourse(@PathParam("courseId") long courseId, Course course) {
+		return courseServiceObject.updateCourse(courseId, course);
+	}
+
+}
